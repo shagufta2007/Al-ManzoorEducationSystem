c98c8c3e7bbdbe2a10f194fc00b72ddd90100700
Shagufta2007 <rshagufta252@gmail.com>
2026-01-31 15:02:11 +0500

    router fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 
 import App from "./App";
 import "./index.css";
@@ -8,8 +9,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
- <HashRouter>
+<BrowserRouter basename="/Al-ManzoorEducationSystem/">
   <App />
-</HashRouter>
+</BrowserRouter>
 
 );
